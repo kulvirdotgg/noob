ec2e7a08e5a11c76fb8dd29e9040bbf06a7eeec9
Kulvir Singh <ksingh11_be20@thapar.edu>
2023-12-21 22:17:05 +0530

    Generate subsets from given input string

---
+++ go-do-again/subsets.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void subsets(string ip, string op, vector<string>& res){
+    if(ip.size() == 0){
+        res.push_back(op);
+        return;
+    }
+    
+    string op1 = op;
+    string op2 = op;
+    op2.push_back(ip[0]);
+    
+    ip.erase(ip.begin()+0);
+    subsets(ip, op1, res);
+    subsets(ip, op2, res);
+}
+
+int main(){
+    vector<string> res;
+    string ip = "ab";
+    
+    subsets(ip,  " ", res);
+    
+    for(string subset: res) cout<<subset<<' ';
+    cout<<'\n';
+}
