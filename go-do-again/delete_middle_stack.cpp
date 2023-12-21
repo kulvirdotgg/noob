b1e220d11eeb3717065a590f7eccf6d55401c41e
Kulvir Singh <ksingh11_be20@thapar.edu>
2023-12-21 20:13:43 +0530

    Delete the middle element of stack

---
+++ go-do-again/delete_middle_stack.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void delete_middle(stack<int>& st, int k){
+    if(k==1) {
+        st.pop();
+        return;
+    }
+    
+    int temp = st.top();
+    st.pop();
+    delete_middle(st,k-1);
+    st.push(temp);
+}
+
+int main(){
+    stack<int> st;
+    st.push(1);
+    st.push(0);
+    st.push(5);
+    st.push(2);
+    
+    delete_middle(st, (st.size()/2)+1);
+    
+    cout<<"Stack after deleting middle element: "<<'\n';
+    while(!st.empty()) {
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    cout<<'\n';
+    
+    return 0;
+}
