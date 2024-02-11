0a9278b7286c7389faf46dd8edffa7ecdcd4b2f4
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-02-11 22:45:57 +0530

    cf-800-3

---
+++ cfp/sherlock.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> solve(int n) {
+        std::vector<int> res(n+2, 1);
+
+        for(int i = 2; i < n+2; i++) {
+            if(res[i] == 2) continue;
+
+            for(int j = 2*i; j < n+2; j += i) {
+                res[j] = 2;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution solution;
+    int n;
+    std::cin >> n;
+
+    if(n > 2) std::cout << 2 << '\n';
+    else std::cout << 1 << '\n';
+
+    std::vector<int> res = solution.solve(n);
+    for(int i = 2; i < n+2; i++) {
+        std::cout << res[i] << ' ';
+    }
+    std::cout << '\n';
+}
