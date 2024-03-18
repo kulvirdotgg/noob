8281cac6e2577dea3562c3fd9a19b29aee522fa4
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-03-19 02:31:07 +0530

    idk

--- test.c
+++ test.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-int i = 0, j = 1, k = 2;
-
-void func(int* p, int* q) {
-    p = q;
-
-    *p = k;
-}
-
-
 int main() {
-    func(&i, &j);
-
-    printf("i = %d, j = %d, \n", i, j);
-
-    return 0;
+    printf("Hello, World!\n");
 }
