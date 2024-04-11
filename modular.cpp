bdf6c7f6cc3e7fc05b39ef903e7deb9b159bcb5a
Kulvir Singh <kulvirs734@gmail.com>
2024-04-11 13:47:25 +0530

    modular exp

---
+++ modular.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int modularExp(std::string num, int k) {
+    int mod = 0;
+
+    for(char strDigit: num) {
+        int digit = strDigit - '0';
+        mod = (mod * 10 + digit) % k;
+    }
+
+    return mod;
+}
+
+int main() {
+    std::string someLargeNum = "2345434534665";
+    int k = 6;
+
+    int res = modularExp(someLargeNum, k);
+    std::cout << res << std::endl;
+    // printf("%s mod k is : %d", someLargeNum, k);
+}
